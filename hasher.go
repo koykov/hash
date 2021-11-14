055bc6c717b7c08e1547acf5c6c0f90665c96f7a
Konstantin Voykov <koykov0@gmail.com>
2021-11-14 18:43:16 +0200

    Declare both 32/64 hashers.

--- hasher.go
+++ hasher.go
@@ -1,6 +1,14 @@
 package hash
 
 // Hasher describes uint64 hash generation from given string.
-type Hasher interface {
+type Hasher Hasher64
+
+// Hasher32 describes uint32 hash generation from given string.
+type Hasher32 interface {
+	Sum32(string) uint32
+}
+
+// Hasher64 describes uint64 hash generation from given string.
+type Hasher64 interface {
 	Sum64(string) uint64
 }
