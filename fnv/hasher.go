0d16b98ff39b1cab4fd75faa2c4895ed0c35976f
Konstantin Voykov <koykov0@gmail.com>
2021-11-14 18:48:16 +0200

    FNV 32/64 hashers.

--- fnv/hasher.go
+++ fnv/hasher.go
@@ -4,9 +4,20 @@ const (
 	longStrThreshold = 256
 )
 
-type Hasher struct{}
+type Hasher Hasher64
 
-func (h Hasher) Sum64(s string) uint64 {
+type Hasher32 struct{}
+
+func (h Hasher32) Sum32(s string) uint32 {
+	if len(s) > longStrThreshold {
+		return Hash32StringLong(s)
+	}
+	return Hash32String(s)
+}
+
+type Hasher64 struct{}
+
+func (h Hasher64) Sum64(s string) uint64 {
 	if len(s) > longStrThreshold {
 		return Hash64StringLong(s)
 	}
