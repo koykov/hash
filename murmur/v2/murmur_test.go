638957cbc6aa365f63f10d0a1c92fbaffba1281b
Konstantin Voykov <koykov0@gmail.com>
2025-02-22 01:44:04 +0300

    bench metrics: add data size in bytes

--- murmur/v2/murmur_test.go
+++ murmur/v2/murmur_test.go
@@ -2,57 +2,66 @@ package murmur
 
 import "testing"
 
-var stages = []struct {
-	data    string
-	seed    uint32
-	seed64  uint64
-	hash    uint32
-	hash64A uint64
-	hash64B uint64
-	hash2A  uint32
-	hashN2  uint32
-	hashA   uint32
-}{
-	{
-		data:    "foo",
-		seed:    0x12345678,
-		hash:    1532637697,
-		hash64A: 14834356025302342401,
-		hash64B: 5484325262697493828,
-		hash2A:  2221687037,
-		hashN2:  1532637697,
-		hashA:   1532637697,
-	},
-	{
-		data:    "foobar",
-		seed:    0x12345678,
-		hash:    151733797,
-		hash64A: 15321041522486911382,
-		hash64B: 4480287663481255131,
-		hash2A:  362873721,
-		hashN2:  151733797,
-		hashA:   151733797,
-	},
-	{
-		data:    "The quick brown fox jumps over the lazy dog",
-		seed:    0x9747b28c,
-		hash:    495243318,
-		hash64A: 6163679885495272987,
-		hash64B: 8470663747738974033,
-		hash2A:  3814933764,
-		hashN2:  495243318,
-		hashA:   495243318,
-	},
-	{
-		data:    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget risus vitae est sagittis euismod. Integer id nibh ut ligula aliquam sagittis.",
-		seed:    0x12345678,
-		hash:    2279665421,
-		hash64A: 928382629942818458,
-		hash64B: 1345853299853536290,
-		hash2A:  507073689,
-		hashN2:  2279665421,
-		hashA:   2279665421,
-	},
+var (
+	stages = []struct {
+		data    string
+		seed    uint32
+		seed64  uint64
+		hash    uint32
+		hash64A uint64
+		hash64B uint64
+		hash2A  uint32
+		hashN2  uint32
+		hashA   uint32
+	}{
+		{
+			data:    "foo",
+			seed:    0x12345678,
+			hash:    1532637697,
+			hash64A: 14834356025302342401,
+			hash64B: 5484325262697493828,
+			hash2A:  2221687037,
+			hashN2:  1532637697,
+			hashA:   1532637697,
+		},
+		{
+			data:    "foobar",
+			seed:    0x12345678,
+			hash:    151733797,
+			hash64A: 15321041522486911382,
+			hash64B: 4480287663481255131,
+			hash2A:  362873721,
+			hashN2:  151733797,
+			hashA:   151733797,
+		},
+		{
+			data:    "The quick brown fox jumps over the lazy dog",
+			seed:    0x9747b28c,
+			hash:    495243318,
+			hash64A: 6163679885495272987,
+			hash64B: 8470663747738974033,
+			hash2A:  3814933764,
+			hashN2:  495243318,
+			hashA:   495243318,
+		},
+		{
+			data:    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget risus vitae est sagittis euismod. Integer id nibh ut ligula aliquam sagittis.",
+			seed:    0x12345678,
+			hash:    2279665421,
+			hash64A: 928382629942818458,
+			hash64B: 1345853299853536290,
+			hash2A:  507073689,
+			hashN2:  2279665421,
+			hashA:   2279665421,
+		},
+	}
+	total int64
+)
+
+func init() {
+	for i := 0; i < len(stages); i++ {
+		total += int64(len(stages[i].data))
+	}
 }
 
 func TestHash(t *testing.T) {
@@ -129,6 +138,7 @@ func TestHashAligned(t *testing.T) {
 
 func BenchmarkHash(b *testing.B) {
 	b.ReportAllocs()
+	b.SetBytes(total)
 	for i := 0; i < b.N; i++ {
 		stage := &stages[b.N%len(stages)]
 		Hash(stage.data, stage.seed)
@@ -137,6 +147,7 @@ func BenchmarkHash(b *testing.B) {
 
 func BenchmarkHash64A(b *testing.B) {
 	b.ReportAllocs()
+	b.SetBytes(total)
 	for i := 0; i < b.N; i++ {
 		stage := &stages[b.N%len(stages)]
 		Hash64A(stage.data, stage.seed64)
@@ -145,6 +156,7 @@ func BenchmarkHash64A(b *testing.B) {
 
 func BenchmarkHash64B(b *testing.B) {
 	b.ReportAllocs()
+	b.SetBytes(total)
 	for i := 0; i < b.N; i++ {
 		stage := &stages[b.N%len(stages)]
 		Hash64B(stage.data, stage.seed64)
@@ -153,6 +165,7 @@ func BenchmarkHash64B(b *testing.B) {
 
 func BenchmarkHash2A(b *testing.B) {
 	b.ReportAllocs()
+	b.SetBytes(total)
 	for i := 0; i < b.N; i++ {
 		stage := &stages[b.N%len(stages)]
 		Hash2A(stage.data, stage.seed)
@@ -161,6 +174,7 @@ func BenchmarkHash2A(b *testing.B) {
 
 func BenchmarkHashNeutral2(b *testing.B) {
 	b.ReportAllocs()
+	b.SetBytes(total)
 	for i := 0; i < b.N; i++ {
 		stage := &stages[b.N%len(stages)]
 		HashNeutral2(stage.data, stage.seed)
@@ -169,6 +183,7 @@ func BenchmarkHashNeutral2(b *testing.B) {
 
 func BenchmarkHashAligned(b *testing.B) {
 	b.ReportAllocs()
+	b.SetBytes(total)
 	for i := 0; i < b.N; i++ {
 		stage := &stages[b.N%len(stages)]
 		HashAligned(stage.data, stage.seed)
--- murmur/v3/murmur_test.go
+++ murmur/v3/murmur_test.go
@@ -2,46 +2,55 @@ package v3
 
 import "testing"
 
-var stages = []struct {
-	data         string
-	seed         uint32
-	seed64       uint64
-	hash         uint32
-	hash_x86_128 [4]uint32
-	hash_x64_128 [2]uint64
-}{
-	{
-		data:         "foo",
-		seed:         0x12345678,
-		seed64:       0x12345678,
-		hash:         4084948345,
-		hash_x86_128: [4]uint32{1918736524, 2601350885, 2601350885, 2601350885},
-		hash_x64_128: [2]uint64{15043494824502257628, 1408089765573727336},
-	},
-	{
-		data:         "foobar",
-		seed:         0x12345678,
-		seed64:       0x12345678,
-		hash:         3074407335,
-		hash_x86_128: [4]uint32{2963197276, 938027862, 3579455989, 3579455989},
-		hash_x64_128: [2]uint64{12751283261750357652, 7917742029045779757},
-	},
-	{
-		data:         "The quick brown fox jumps over the lazy dog",
-		seed:         0x9747b28c,
-		seed64:       0x9747b28c,
-		hash:         3647120414,
-		hash_x86_128: [4]uint32{2329204062, 1287151985, 2393324188, 3451287870},
-		hash_x64_128: [2]uint64{8325606756057297185, 17961889624427075301},
-	},
-	{
-		data:         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget risus vitae est sagittis euismod. Integer id nibh ut ligula aliquam sagittis.",
-		seed:         0x12345678,
-		seed64:       0x12345678,
-		hash:         991832159,
-		hash_x86_128: [4]uint32{368352206, 1111628908, 560953716, 1558498190},
-		hash_x64_128: [2]uint64{1320662463624528565, 6505946595269923697},
-	},
+var (
+	stages = []struct {
+		data         string
+		seed         uint32
+		seed64       uint64
+		hash         uint32
+		hash_x86_128 [4]uint32
+		hash_x64_128 [2]uint64
+	}{
+		{
+			data:         "foo",
+			seed:         0x12345678,
+			seed64:       0x12345678,
+			hash:         4084948345,
+			hash_x86_128: [4]uint32{1918736524, 2601350885, 2601350885, 2601350885},
+			hash_x64_128: [2]uint64{15043494824502257628, 1408089765573727336},
+		},
+		{
+			data:         "foobar",
+			seed:         0x12345678,
+			seed64:       0x12345678,
+			hash:         3074407335,
+			hash_x86_128: [4]uint32{2963197276, 938027862, 3579455989, 3579455989},
+			hash_x64_128: [2]uint64{12751283261750357652, 7917742029045779757},
+		},
+		{
+			data:         "The quick brown fox jumps over the lazy dog",
+			seed:         0x9747b28c,
+			seed64:       0x9747b28c,
+			hash:         3647120414,
+			hash_x86_128: [4]uint32{2329204062, 1287151985, 2393324188, 3451287870},
+			hash_x64_128: [2]uint64{8325606756057297185, 17961889624427075301},
+		},
+		{
+			data:         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget risus vitae est sagittis euismod. Integer id nibh ut ligula aliquam sagittis.",
+			seed:         0x12345678,
+			seed64:       0x12345678,
+			hash:         991832159,
+			hash_x86_128: [4]uint32{368352206, 1111628908, 560953716, 1558498190},
+			hash_x64_128: [2]uint64{1320662463624528565, 6505946595269923697},
+		},
+	}
+	total int64
+)
+
+func init() {
+	for i := 0; i < len(stages); i++ {
+		total += int64(len(stages[i].data))
+	}
 }
 
 func equal_x86_128(a, b [4]uint32) bool {
@@ -90,6 +99,7 @@ func TestHash_x64_128(t *testing.T) {
 
 func BenchmarkHash(b *testing.B) {
 	b.ReportAllocs()
+	b.SetBytes(total)
 	for i := 0; i < b.N; i++ {
 		stage := &stages[b.N%len(stages)]
 		Hash(stage.data, stage.seed)
@@ -98,6 +108,7 @@ func BenchmarkHash(b *testing.B) {
 
 func BenchmarkHash_x86_128(b *testing.B) {
 	b.ReportAllocs()
+	b.SetBytes(total)
 	for i := 0; i < b.N; i++ {
 		stage := &stages[b.N%len(stages)]
 		Hash_x86_128(stage.data, stage.seed)
@@ -106,6 +117,7 @@ func BenchmarkHash_x86_128(b *testing.B) {
 
 func BenchmarkHash_x64_128(b *testing.B) {
 	b.ReportAllocs()
+	b.SetBytes(total)
 	for i := 0; i < b.N; i++ {
 		stage := &stages[b.N%len(stages)]
 		Hash_x64_128(stage.data, stage.seed64)
