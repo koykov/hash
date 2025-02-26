798150afc7bb249130861f0916d3bba4b20946f4
Konstantin Voykov <koykov0@gmail.com>
2025-02-26 21:49:34 +0300

    fnv: remove loop unrolling methods

--- fnv2/hasher.go
+++ fnv2/hasher.go
@@ -1,41 +1,25 @@
 package fnv
 
-const (
-	longStrThreshold = 256
-)
-
 type Hasher32[T byteseq] struct{}
 
 func (h Hasher32[T]) Sum32(s T) uint32 {
-	if len(s) > longStrThreshold {
-		return Hash32Long(s)
-	}
 	return Hash32(s)
 }
 
 type Hasher64[T byteseq] struct{}
 
 func (h Hasher64[T]) Sum64(s T) uint64 {
-	if len(s) > longStrThreshold {
-		return Hash64Long(s)
-	}
 	return Hash64(s)
 }
 
 type Hasher32a[T byteseq] struct{}
 
 func (h Hasher32a[T]) Sum32(s T) uint32 {
-	if len(s) > longStrThreshold {
-		return Hash32aLong(s)
-	}
 	return Hash32a(s)
 }
 
 type Hasher64a[T byteseq] struct{}
 
 func (h Hasher64a[T]) Sum64(s T) uint64 {
-	if len(s) > longStrThreshold {
-		return Hash64aLong(s)
-	}
 	return Hash64a(s)
 }
